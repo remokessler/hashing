58b1e9a2eb52912b12de1429be7ff057d153f5ba
marco <marco.ebneter@gmail.com>
2020-11-30 18:54:20 +0100

    pref

---
+++ SortComparison/SortComparison/SortingAlgorithms/ShellSort.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace SortComparison.SortingAlgorithms
+{
+    public class ShellSort : SortAlgorithm
+    {
+        public override string Name => "Shellsort";
+
+        public override void Sort(IList<int> arrayToSort)
+        {
+            int i, j, inc, temp;
+            inc = arrayToSort.Count / 4;
+            while (inc > 0)
+            {
+                for (i = 0; i < arrayToSort.Count; i++)
+                {
+                    j = i;
+                    temp = arrayToSort[i];
+                    while ((j >= inc) && (arrayToSort[j - inc] > temp))
+                    {
+                        arrayToSort[j] = arrayToSort[j - inc];
+                        j = j - inc;
+                    }
+                    arrayToSort[j] = temp;
+                }
+                if (inc / 2 != 0)
+                {
+                    inc = inc / 2;
+                }
+                else if (inc == 1)
+                {
+                    inc = 0;
+                }
+                else
+                {
+                    inc = 1;
+                }
+            }
+        }
+    }
+}
